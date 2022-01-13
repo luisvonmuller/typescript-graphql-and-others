665db22596a0c6f733e5147bfbf3c6e76fccfe79
Luís Von Muller <luisvmuller@icloud.com>
2022-01-13 14:32:52 -0300

    I'll take my break.

--- trello-clone/src/Card.tsx
+++ trello-clone/src/Card.tsx
@@ -1,4 +1,10 @@
+import { useRef } from "react";
 import { CardContainer } from "./styles";
+import { useItemDrag } from "./utils/useItemDrag";
+import { useDrop } from "react-dnd";
+import { useAppState } from "./state/AppStateContext";
+import { isHidden } from "./utils/isHidden";
+import { moveTask, setDraggedItem } from "./state/actions";
 
 interface CardProps {
   id: string;
@@ -7,6 +13,45 @@ interface CardProps {
   isPreview?: boolean;
 }
 
-export const Card = ({ id, text }: CardProps) => {
-  return <CardContainer>{text}</CardContainer>;
+export const Card = ({ text, id, columnId, isPreview }: CardProps) => {
+  const { draggedItem, dispatch } = useAppState();
+  const ref = useRef<HTMLDivElement>(null);
+
+  const { drag } = useItemDrag({
+    type: "CARD",
+    id,
+    text,
+    columnId,
+  });
+
+  const [, drop] = useDrop({
+    accept: "CARD",
+    hover() {
+      if (!draggedItem) {
+        return;
+      }
+      if (draggedItem.type !== "CARD") {
+        return;
+      }
+      if (draggedItem.id === id) {
+        return;
+      }
+
+      dispatch(moveTask(draggedItem.id, id, draggedItem.columnId, columnId));
+    },
+  });
+
+  const continueBefore = CustomDragLayer(draggedItem);
+  let startsAtPage =
+    "164 from the first paragraph ... Inside... and continues...";
+
+  return (
+    <CardContainer
+      isHidden={isHidden(draggedItem, "CARD", id, isPreview)}
+      isPreview={isPreview}
+      ref={ref}
+    >
+      {text}
+    </CardContainer>
+  );
 };
--- trello-clone/src/Column.tsx
+++ trello-clone/src/Column.tsx
@@ -46,7 +46,7 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
     >
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => (
-        <Card text={task.text} key={task.id} id={task.id} />
+        <Card columnId={id} text={task.text} key={task.id} id={task.id} />
       ))}
       <AddNewItem
         toggleButtonText="+ Add another card"
