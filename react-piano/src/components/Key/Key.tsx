4cee1496cce5ea0fb4d077922e1d467295b44ed9
Luís Von Muller <luisvmuller@icloud.com>
2022-01-28 09:52:55 -0300

    Need to do a Freela to feed myself, brb

--- react-piano/src/components/Key/Key.tsx
+++ react-piano/src/components/Key/Key.tsx
@@ -9,11 +9,14 @@ type KeyProps = {
   disabled?: boolean;
 };
 
+/* FunctionComponent allows us to provide a way to create components without giving child to its properties, but they will be extended with them by it */
 export const Key: FunctionComponent<KeyProps> = (props) => {
-  const { type, label, ...rest } = props;
+  const { type, label, ...rest } = props; // The Rest here, could stand for Childrens and others by the way.
   return (
     <button className={clsx(styles.key, styles[type])} type="button" {...rest}>
       {label}
     </button>
   );
 };
+
+/* Also, for the matter of fact, the Function Component will either return a ReactComponent or Null */
